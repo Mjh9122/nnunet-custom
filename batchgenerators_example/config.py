3142ce372e463188831228bd21d90f4315f00447
mjh9122 <mjh9122@rit.edu>
2025-06-20 14:09:48 -0400

    Modified preprocessing for decathlon formating

--- batchgenerators_example/config.py
+++ batchgenerators_example/config.py
@@ -1,3 +1,3 @@
 brats_preprocessed_folder = "/mnt/d/batchgen/Brats_preprocessed"
-brats_folder_with_downloaded_train_data = "/mnt/d/Task01_BrainTumour"
+brats_folder_with_downloaded_train_data = "/mnt/d/Task01_BrainTumour/imagesTr"
 num_threads_for_brats_example = 8
