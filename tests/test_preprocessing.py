f13981ac6934ee95fead619df83d2914173a9383
mjh9122 <mjh9122@rit.edu>
2025-06-22 17:10:19 -0400

    Implement and test resample image

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -8,9 +8,11 @@ import numpy as np
 src_path = Path(__file__).parent.parent / "src"
 sys.path.insert(0, str(src_path))
 
-from preprocessing import load_data, crop_zeros, modality_detection
+from preprocessing import load_data, crop_zeros, modality_detection, resample_image
 
 TEST_DATA_DIR = Path(__file__).parent / "test_data"
+
+np.random.seed(42)
 
 
 def test_load_data_nonexistant_file():
@@ -78,3 +80,102 @@ def test_modality_detection(input, expected):
 def test_modality_dectection_file_nonexistent():
     with pytest.raises(FileNotFoundError):
         modality_detection(TEST_DATA_DIR / "dataset_05.json")
+
+@pytest.mark.parametrize(
+        'image, old_spacing, new_spacing',
+        (
+            (np.ones((4, 4, 4)), np.array((1., 1., 1.)), np.array((2., 2.))),
+            (np.ones((4, 4, 4, 4)), np.array((1., 1., 1.)), np.array((2., 2., 2.)))
+        )
+)
+def test_resample_image_bad_spacings(image, old_spacing, new_spacing):
+    with pytest.raises(Exception):
+        resample_image(image, old_spacing, new_spacing, is_segmentation = False)
+
+@pytest.mark.parametrize(
+    'old_dims, old_spacing, new_spacing, expected',
+    (
+        ((16, 32, 32), (2., 1., 1.), (2., 2., 2.), (16, 16, 16)),
+        ((16, 16, 16), (.33, .33, .33), (.33, .33, .33), (16, 16, 16)),
+        ((100, 100, 50), (1., 1., 2.), (.5, .5, 1.), (200, 200, 100)),
+        ((100, 100, 50), (1., 1., 2.), (10., 10., 10.), (10, 10, 10)),
+        ((10, 10, 10), (10., 10., 10.), (1., 1., 2.), (100, 100, 50))
+    )
+)
+def test_resample_image_expected_dims(old_dims, old_spacing, new_spacing, expected):
+    img = np.random.rand(*old_dims)
+    reshaped = resample_image(img, old_spacing, new_spacing, is_segmentation = False)
+
+    assert expected == reshaped.shape
+
+def test_rasample_image_range_preservation():
+    old_dims = (64, 32, 32)
+    old_spacing = (1., .5, .5)
+    new_spacing = (1.5, .75, .75)
+    img = np.random.rand(*old_dims) * 1000
+
+    reshaped = resample_image(img, old_spacing, new_spacing, is_segmentation = False)
+    
+    tol = .05 * (img.max() - img.min())
+    assert reshaped.min() > img.min() - tol
+    assert reshaped.max() < img.max() + tol
+
+def test_resample_image_energy_preservation():
+    old_dims = (64, 32, 32)
+    old_spacing = (1., .5, .5)
+    new_spacing = (1., 1., 1.)
+    img = np.random.rand(*old_dims) * 1000
+
+    reshaped = resample_image(img, old_spacing, new_spacing, is_segmentation = False)
+    
+    tol = .005
+    sum_diff = abs(img.sum() * np.prod(old_spacing) - reshaped.sum() * np.prod(new_spacing))
+
+    assert  sum_diff / img.sum() * np.prod(old_spacing) < tol
+
+def test_resample_image_uniform():
+    const = 1000
+    old_dims = (64, 32, 32)
+    old_spacing = (5., .1, .1)
+    new_spacing = (1., 1., 1.)
+    img = np.ones(old_dims) * const
+
+    reshaped = resample_image(img, old_spacing, new_spacing, is_segmentation = False)
+
+    err = np.abs(reshaped - const)
+    rtol = .01
+
+    assert np.max(err) < rtol * const
+
+def test_resample_image_seg_mask_up():
+    img = np.array([[[1], [0]], [[0], [1]]])
+    old_spacing  = (2, 2, 2)
+    new_spacing = (1, 1, 2)
+
+    expected = np.array([
+        [[1], [1], [0], [0]], 
+        [[1], [1], [0], [0]],
+        [[0], [0], [1], [1]],
+        [[0], [0], [1], [1]]
+    ])
+
+    reshaped = resample_image(img, old_spacing, new_spacing, True)
+
+    np.testing.assert_array_equal(expected, reshaped)
+
+def test_resample_image_seg_mask_down():
+    img = np.array([
+        [[1], [1], [0], [0]], 
+        [[1], [1], [0], [0]],
+        [[0], [0], [1], [1]],
+        [[0], [0], [1], [1]]
+    ])
+    old_spacing  = (1, 1, 2)
+    new_spacing = (2, 2, 2)
+
+    expected = np.array([[[1], [0]], [[0], [1]]])
+
+    reshaped = resample_image(img, old_spacing, new_spacing, True)
+
+    np.testing.assert_array_equal(expected, reshaped)
+
