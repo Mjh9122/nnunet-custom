11b59a59d9600d2fdab0cde160bacb08c4b1af18
mjh9122 <mjh9122@rit.edu>
2025-06-27 22:42:07 -0400

    Crop testing is now beautiful

--- tests/preprocessing/test_crop.py
+++ tests/preprocessing/test_crop.py
@@ -1,14 +1,29 @@
+import warnings
+
+warnings.filterwarnings(
+    "ignore", category=DeprecationWarning, module="importlib._bootstrap"
+)
+
 import sys
 from pathlib import Path
 
 import numpy as np
+import os
 import pytest
+import SimpleITK as sitk
+import shutil
+
 
 src_path = Path(__file__).parent.parent.parent / "src"
 sys.path.insert(0, str(src_path))
 
-from preprocessing.crop import crop_zeros
+from preprocessing.crop import crop_zeros, crop_dataset
 
+
+
+TEST_DATA_DIR = Path(__file__).parent / "crop_data"
+
+np.random.seed(42)
 
 @pytest.mark.parametrize(
     "input_dims, ones_slices, output_dims",
@@ -34,3 +49,64 @@ def test_crop_bool(input_dims, ones_slices, output_dims):
     cropped_arr, cropped_mask = crop_zeros(arr, seg_mask)
     np.testing.assert_array_equal(cropped_arr, np.ones(output_dims, np.float32))
     np.testing.assert_array_equal(cropped_mask, np.ones(output_dims, np.float32))
+
+def setup_dataset():
+    if os.path.exists(TEST_DATA_DIR):
+        if not os.path.isdir(TEST_DATA_DIR):
+            raise Exception(f'Test data path {TEST_DATA_DIR} is not a directory')
+    else:
+        os.mkdir(TEST_DATA_DIR)
+        os.mkdir(TEST_DATA_DIR / 'imagesTr')
+        os.mkdir(TEST_DATA_DIR / 'labelsTr')
+
+    arrs = [np.zeros((10, 10, 10), np.float32) for _ in range(10)]
+    masks = [np.zeros((10, 10, 10), np.float32) for _ in range(10)]
+
+    ones = np.random.randint(low = 0, high = 10, size = (10, 3, 3))
+
+    for arr, spots in zip(arrs, ones):
+        for spot in spots:
+            arr[spot[0], spot[1], spot[2]] = 1
+    
+    crops = [[(min(spots[:, n]), max(spots[:, n])) for n in range(3)] for spots in ones]
+        
+
+    imgs = [sitk.GetImageFromArray(arr) for arr in arrs]
+    for i, img in enumerate(imgs):
+        sitk.WriteImage(img, TEST_DATA_DIR / f"imagesTr/test0{i}.nii.gz")
+
+    imgs = [sitk.GetImageFromArray(mask) for mask in masks]
+
+    for i, img in enumerate(imgs):
+        sitk.WriteImage(img, TEST_DATA_DIR / f"labelsTr/test0{i}.nii.gz")
+
+    return crops
+
+def tear_down_dataset():
+    """ crop_data
+        -> crops
+            -> imagesTr
+            -> labelsTr
+        -> imagesTr
+        -> labelsTr
+    """ 
+    shutil.rmtree(TEST_DATA_DIR)
+
+def test_crop_dataset():
+    crops = setup_dataset()
+    results = crop_dataset(TEST_DATA_DIR, TEST_DATA_DIR)
+
+    images = sorted([f for f in os.listdir(results / 'imagesTr') if f[-3:] != 'pkl'])
+    for crops, img in zip(crops, images):
+        cropped_img = sitk.ReadImage(results / 'imagesTr' / img)
+        cropped_mask = sitk.ReadImage(results / 'labelsTr' / img)
+
+        img_size = np.array(cropped_img.GetSize()[::-1])
+        mask_size = np.array(cropped_mask.GetSize()[::-1])
+
+        correct_size = [(b - a + 1) for a, b in crops]
+
+        np.testing.assert_array_equal(img_size, mask_size)
+        np.testing.assert_array_equal(img_size, correct_size)
+    
+    tear_down_dataset()
